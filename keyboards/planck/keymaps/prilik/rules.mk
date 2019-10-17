81923f23edff53e17a7f63e7167b17bcd1024bbd
Daniel Prilik <danielprilik@gmail.com>
2019-10-17 14:10:13 -0700

    snake

--- keyboards/planck/keymaps/prilik/rules.mk
+++ keyboards/planck/keymaps/prilik/rules.mk
@@ -1,7 +1,12 @@
 SRC += muse.c
+
+EXTRAFLAGS += -DRGB_MATRIX_STARTUP_MODE=RGB_MATRIX_CUSTOM_snake
+
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 LINK_TIME_OPTIMIZATION_ENABLE = yes
 COMMAND_ENABLE = no
 MOUSEKEY_ENABLE = no
 UNICODEMAP_ENABLE = yes
+
+RGB_MATRIX_CUSTOM_USER = yes
